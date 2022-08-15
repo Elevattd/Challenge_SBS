a93b4b2d2b4b321182e4408448421b9fffcf75fb
Elevattd <marianoo.14.md@gmail.com>
2022-08-15 18:18:34 -0300

    feat(clientA): add modal details

---
+++ Client_A/src/app/components/ProductDetails/ProductDetails.tsx
@@ -0,0 +1,59 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Modal,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "auto",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const ProductDetails = ({ open, handleClose, product }: any) => {
+  console.log(product);
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Card sx={style}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Cerrar</Button>
+          </CardActions>
+        </Card>
+      </Modal>
+    </div>
+  );
+};
+
+export default ProductDetails;
--- Client_A/src/app/components/ProductList/ProductList.tsx
+++ Client_A/src/app/components/ProductList/ProductList.tsx
@@ -1,8 +1,17 @@
 import { Button } from "@mui/material";
+import { useState } from "react";
 import { useAppSelector } from "../../hooks";
+import ProductDetails from "../ProductDetails/ProductDetails";
 
 const ProductList = () => {
   const products = useAppSelector((state) => state.products.products);
+  const [open, setOpen] = useState(false);
+  const [productId, setProductId] = useState("");
+
+  const handleClick = async (productId: string) => {
+    setProductId(productId);
+    setOpen(true);
+  };
 
   const productList: any = products
     ? products?.map((product: any) => (
@@ -11,14 +20,33 @@ const ProductList = () => {
           <h1>
             Nombre: {product.name[0].toUpperCase() + product.name.slice(1)}
           </h1>
-          <img src={product.image} alt={product.name} />
+          <img src={product.image} alt={product.name} height="140" />
           <p>Descripcion: {product.description.slice(0, 60)}...</p>
           <h3>Precio: ${product.price},00</h3>
-          <Button>Más info</Button>
+          <Button
+            onClick={() => {
+              handleClick(product.id);
+            }}
+          >
+            Más info
+          </Button>
         </div>
       ))
     : null;
-  return <div>{products && productList}</div>;
+  return (
+    <div>
+      {products && productList}
+      {open && (
+        <ProductDetails
+          open={open}
+          handleClose={() => {
+            setOpen(false);
+          }}
+          product={productId}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ProductList;
