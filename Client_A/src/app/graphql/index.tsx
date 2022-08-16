dde9ff383cc198de16691b7201ca903847d8f491
Elevattd <marianoo.14.md@gmail.com>
2022-08-15 23:07:38 -0300

    fix(clientA): deploy fix, enviroments variables

--- Client_A/src/app/graphql/index.tsx
+++ Client_A/src/app/graphql/index.tsx
@@ -1,11 +1,14 @@
 import { GraphQLClient } from "graphql-request";
 import io from "socket.io-client";
 
-const API = process.env.REACT_APP_API_URL;
-
-export const graphQLClient = new GraphQLClient(`${API}graphql`, {
-  headers: {},
-});
+export const graphQLClient = new GraphQLClient(
+  `${process.env.REACT_APP_API_URL}graphql`,
+  {
+    headers: {},
+  }
+);
 
 // Socket io connection
-export const socket = io("https://challenge-sbs.herokuapp.com/");
+export const socket = io(
+  process.env.REACT_APP_API_URL || "http://localhost:4000"
+);
