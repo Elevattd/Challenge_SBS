c050e587d24637959a9f841467111ce0797842c0
Elevattd <marianoo.14.md@gmail.com>
2022-08-15 22:44:15 -0300

    refactor(clientA): refactor Enviroments for deploy

--- Client_A/src/app/graphql/index.tsx
+++ Client_A/src/app/graphql/index.tsx
@@ -3,9 +3,9 @@ import io from "socket.io-client";
 
 const API_URL = process.env.API_URL;
 
-export const graphQLClient = new GraphQLClient(API_URL + "/graphql", {
+export const graphQLClient = new GraphQLClient(API_URL + "graphql", {
   headers: {},
 });
 
 // Socket io connection
-export const socket = io(API_URL || "http://localhost:4000/");
+export const socket = io("https://challenge-sbs.herokuapp.com/");
