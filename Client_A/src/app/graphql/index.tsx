70f53941be628314578e5c64250359453dbe6018
Elevattd <marianoo.14.md@gmail.com>
2022-08-15 22:55:43 -0300

    fix(clientA&B): refactor enviroments variables

--- Client_A/src/app/graphql/index.tsx
+++ Client_A/src/app/graphql/index.tsx
@@ -1,13 +1,9 @@
 import { GraphQLClient } from "graphql-request";
 import io from "socket.io-client";
 
-const API_URL = process.env.API_URL;
-
 export const graphQLClient = new GraphQLClient(
   "https://challenge-sbs.herokuapp.com/graphql",
-  {
-    headers: {},
-  }
+  { headers: {} }
 );
 
 // Socket io connection
--- Client_B/src/app/graphql/index.tsx
+++ Client_B/src/app/graphql/index.tsx
@@ -2,9 +2,9 @@ import { GraphQLClient } from "graphql-request";
 import io from "socket.io-client";
 
 export const graphQLClient = new GraphQLClient(
-  "http://localhost:4000/graphql",
+  "https://challenge-sbs.herokuapp.com/graphql",
   { headers: {} }
 );
 
 // Socket io connection
-export const socket = io("http://localhost:4000");
+export const socket = io("https://challenge-sbs.herokuapp.com/");
