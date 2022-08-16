e75cb41fb650714ef40b1eb8ab94464a5a8deeb1
Elevattd <marianoo.14.md@gmail.com>
2022-08-16 04:20:02 -0300

    feat(clientA): add footer styles

--- Client_A/src/app/components/Footer/Footer.tsx
+++ Client_A/src/app/components/Footer/Footer.tsx
@@ -7,51 +7,56 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <Box sx={{ bgcolor: "common.black", color: "grey.700" }}>
-        <Container maxWidth="md" sx={{ py: 6 }}>
-          <Typography variant="body1" color="white">
-            Mariano Dunand
-          </Typography>
-          <br />
-          <Typography variant="body1">Fecha deploy 15/08/2022</Typography>
-          <br />
-          <Grid container spacing={5}>
-            <Grid item xs={6} md={3}>
-              <Stack spacing={2}>
-                <a href="https://github.com/Elevattd" target="_blank">
-                  <GitHubIcon className="link" />
-                </a>
-              </Stack>
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <Stack spacing={2}>
-                <a
-                  href="https://www.linkedin.com/in/mariano-dunand/"
-                  target="_blank"
-                >
-                  <LinkedInIcon className="link" />
-                </a>
-              </Stack>
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <Stack spacing={2}>
-                <a href="https://mariano-dunand.vercel.app/" target="_blank">
-                  <PermContactCalendarIcon className="link" />
-                </a>
-              </Stack>
-            </Grid>
-            <Grid item xs={6} md={3}>
-              <Stack spacing={2}>
-                <a href="https://www.instagram.com/marianoo9" target="_blank">
-                  <InstagramIcon className="link" />
-                </a>
-              </Stack>
-            </Grid>
+    <Box
+      sx={{
+        bgcolor: "common.black",
+        color: "grey.700",
+        margin: "0",
+        padding: "0",
+      }}
+    >
+      <Container maxWidth="md" sx={{ py: 6 }}>
+        <Typography variant="body1" color="white">
+          Mariano Dunand
+        </Typography>
+        <br />
+        <Typography variant="body1">Fecha deploy 15/08/2022</Typography>
+        <br />
+        <Grid container spacing={5}>
+          <Grid item xs={6} md={3}>
+            <Stack spacing={2}>
+              <a href="https://github.com/Elevattd" target="_blank">
+                <GitHubIcon className="link" />
+              </a>
+            </Stack>
           </Grid>
-        </Container>
-      </Box>
-    </footer>
+          <Grid item xs={6} md={3}>
+            <Stack spacing={2}>
+              <a
+                href="https://www.linkedin.com/in/mariano-dunand/"
+                target="_blank"
+              >
+                <LinkedInIcon className="link" />
+              </a>
+            </Stack>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <Stack spacing={2}>
+              <a href="https://mariano-dunand.vercel.app/" target="_blank">
+                <PermContactCalendarIcon className="link" />
+              </a>
+            </Stack>
+          </Grid>
+          <Grid item xs={6} md={3}>
+            <Stack spacing={2}>
+              <a href="https://www.instagram.com/marianoo9" target="_blank">
+                <InstagramIcon className="link" />
+              </a>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
